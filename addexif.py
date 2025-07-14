e51ce2f3d4bbfcdc7e1dd0fc4265bb68b211a3ed
Ian Page Hands <iphands@gmail.com>
2025-07-14 09:34:25 -0700

    :lipstick: linting only

--- addexif.py
+++ addexif.py
@@ -6,36 +6,44 @@ from exif import Image
 from datetime import datetime
 from datetime import timedelta
 
-class BadCzoomMatch(Exception): pass
+
+class BadCzoomMatch(Exception):
+    pass
+
 
 # dt = datetime.fromtimestamp(int(unix_stamp))
 
 # CameraZOOM-20160114233054379.jpg
-czoom    = re.compile('.*CameraZOOM-([0-9]{14}).*jpg')
-czoom_24 = re.compile('([0-9]{8}([0-9]{2}))')
+czoom = re.compile(".*CameraZOOM-([0-9]{14}).*jpg")
+czoom_24 = re.compile("([0-9]{8}([0-9]{2}))")
+
 
 def try_czoom_fix(s):
     m = czoom_24.match(s)
     if m.group(2) == "24":
         new = m.group(1)[:-2] + "23"
-        s   = s.replace(m.group(1), new)
+        s = s.replace(m.group(1), new)
         return (s, True)
     return (s, False)
 
+
 def handle_czoom(f, stamp):
     stamp, fix = try_czoom_fix(stamp)
-    dt = datetime.strptime(stamp, '%Y%m%d%H%M%S')
-    if fix: dt = dt + timedelta(hours=1)
+    dt = datetime.strptime(stamp, "%Y%m%d%H%M%S")
+    if fix:
+        dt = dt + timedelta(hours=1)
 
-    with open(f, 'rb') as image_file:
+    with open(f, "rb") as image_file:
         my_image = Image(image_file)
         print(dir(my_image))
-        print(my_image['datetime'])
+        print(my_image["datetime"])
     print(dt)
 
 
 # /1463171219540.jpg
-unixlike = re.compile('.*/(1[3-4][0-9]{8})[0-9]*.*jpg', re.IGNORECASE)
+unixlike = re.compile(".*/(1[3-4][0-9]{8})[0-9]*.*jpg", re.IGNORECASE)
+
+
 def try_unixlike(f):
     res = unixlike.match(f)
     if res:
@@ -43,8 +51,11 @@ def try_unixlike(f):
         return True
     return False
 
+
 # /FB_IMG_1442980032538.jpg
-fbre = re.compile('.*/FB_IMG_(1[3-4][0-9]{8})[0-9]*.*jpg', re.IGNORECASE)
+fbre = re.compile(".*/FB_IMG_(1[3-4][0-9]{8})[0-9]*.*jpg", re.IGNORECASE)
+
+
 def try_fb(f):
     res = fbre.match(f)
     if res:
@@ -52,50 +63,73 @@ def try_fb(f):
         return True
     return False
 
+
 # /2010-01-28-174545.jpg
-webcamre = re.compile('.*/(2[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{6}).*jpg', re.IGNORECASE)
+webcamre = re.compile(".*/(2[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{6}).*jpg", re.IGNORECASE)
+
+
 def try_webcam_date(f):
     res = webcamre.match(f)
     if res:
-        dt = datetime.strptime(res.group(1), '%Y-%m-%d-%H%M%S')
+        dt = datetime.strptime(res.group(1), "%Y-%m-%d-%H%M%S")
         write_file(dt, f)
         return True
     return False
 
+
 # /IMG_20130405_195524.jpg
-imgre = re.compile('.*([0-9]{8}_[0-9]{6}).*jpg', re.IGNORECASE)
+imgre = re.compile(".*([0-9]{8}_[0-9]{6}).*jpg", re.IGNORECASE)
+
+
 def try_img_date(f):
     res = imgre.match(f)
     if res:
-        dt = datetime.strptime(res.group(1), '%Y%m%d_%H%M%S')
+        dt = datetime.strptime(res.group(1), "%Y%m%d_%H%M%S")
         write_file(dt, f)
         return True
     return False
 
+
 # /mnt/nas/pics/old/sorted/2015/12/BABY ADRIAN_45.JPG
-sortedre = re.compile('.*/([0-9]{4}/[0-9]{2}).*jpg', re.IGNORECASE)
+sortedre = re.compile(".*/([0-9]{4}/[0-9]{2}).*jpg", re.IGNORECASE)
+
+
 def try_folder(f):
     res = sortedre.match(f)
     if res:
-        dt = datetime.strptime(res.group(1), '%Y/%m')
+        dt = datetime.strptime(res.group(1), "%Y/%m")
         write_file(dt, f)
         return True
     return False
 
+
 def write_file(dt, f):
-        command = 'exiv2 -M "set Exif.Image.DateTime ' + dt.strftime("%Y:%m:%d %H:%M:%S") + '" "' + f + '"'
-        print(f)
-        # print(command)
-        # os.system(command)
+    command = (
+        'exiv2 -M "set Exif.Image.DateTime '
+        + dt.strftime("%Y:%m:%d %H:%M:%S")
+        + '" "'
+        + f
+        + '"'
+    )
+    print(f)
+    # print(command)
+    # os.system(command)
+
 
 def tryfile(f):
-    if try_unixlike(f): return
-    if try_fb(f): return
-    if try_webcam_date(f): return
-    if try_img_date(f): return
-    if try_folder(f): return
+    if try_unixlike(f):
+        return
+    if try_fb(f):
+        return
+    if try_webcam_date(f):
+        return
+    if try_img_date(f):
+        return
+    if try_folder(f):
+        return
 
-with open('tmp/all', 'r') as lst:
+
+with open("tmp/all", "r") as lst:
     line = lst.readline().strip()
     while line:
         if len(line) > 4:
--- sort.py
+++ sort.py
@@ -11,46 +11,63 @@ from datetime import datetime
 from exif import Image
 from plum.exceptions import UnpackError
 
-class NoDateTime(Exception): pass
-class NoExif(Exception): pass
-class NoValidDate(Exception): pass
-class UnsupportedSuffix(Exception): pass
 
-DRY=False
+class NoDateTime(Exception):
+    pass
+
+
+class NoExif(Exception):
+    pass
+
+
+class NoValidDate(Exception):
+    pass
+
+
+class UnsupportedSuffix(Exception):
+    pass
+
+
+DRY = False
+
 
 def md5(fname):
-    with open(fname, 'rb') as f:
+    with open(fname, "rb") as f:
         hash_md5 = hashlib.md5()
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
         return hash_md5.hexdigest()
 
+
 def try_gps_date(img):
-    if 'gps_datestamp' in dir(img):
-        dt = img['gps_datestamp'].split(':')
+    if "gps_datestamp" in dir(img):
+        dt = img["gps_datestamp"].split(":")
         data = {
-            "year":  dt[0],
+            "year": dt[0],
             "month": dt[1],
-            "day":   dt[2],
-            "hour":  "00",
-            "min":   "00",
-            "sec":   "00"
+            "day": dt[2],
+            "hour": "00",
+            "min": "00",
+            "sec": "00",
         }
         return data
     return False
 
+
 def try_datetime(img, key):
-    if key not in img and not key in dir(img): return
-    dt   = img[key].split(':')
+    if key not in img and not key in dir(img):
+        return
+    dt = img[key].split(":")
     data = {
-        "year":  dt[0],
+        "year": dt[0],
         "month": dt[1],
-        "day":   dt[2].split(' ')[0],
-        "hour":  dt[2].split(' ')[1],
-        "min":   dt[3],
-        "sec":   dt[4]
+        "day": dt[2].split(" ")[0],
+        "hour": dt[2].split(" ")[1],
+        "min": dt[3],
+        "sec": dt[4],
     }
     return data
+
 
 def try_from_stat(img):
     stat = os.stat(img)
@@ -66,28 +83,37 @@ def try_from_stat(img):
         if i > min_date:
             date = i
             break
-    if date == None: raise NoValidDate(f"Cant find valid date in {img}: {dates}")
+    if date == None:
+        raise NoValidDate(f"Cant find valid date in {img}: {dates}")
     return _data_from_datetime(str(date))
+
 
 def _get_data_norm(f, image_file):
     try:
         my_image = Image(image_file)
     except UnpackError:
         data = try_from_stat(f)
-        if not data: raise e
+        if not data:
+            raise e
         return data
 
     if not my_image.has_exif:
         data = try_from_stat(f)
-        if not data: raise NoExif()
+        if not data:
+            raise NoExif()
         return data
 
     data = try_gps_date(my_image)
-    if not data: data = try_datetime(my_image, 'datetime_original')
-    if not data: data = try_datetime(my_image, 'datetime')
-    if not data: data = try_from_stat(f)
-    if not data: raise NoDateTime()
+    if not data:
+        data = try_datetime(my_image, "datetime_original")
+    if not data:
+        data = try_datetime(my_image, "datetime")
+    if not data:
+        data = try_from_stat(f)
+    if not data:
+        raise NoDateTime()
     return data
+
 
 def _data_from_datetime(dt):
     dt = dt.split(".")[0]
@@ -104,54 +130,59 @@ def _data_from_datetime(dt):
         "sec": str(parsed.second).zfill(2),
     }
 
+
 def _get_data_ffmpeg(fil):
-    js = ffmpeg.probe(fil, show_entries='format')
-    if 'format' in js:
-        if 'tags' in js['format']:
-            if 'creation_time' in js['format']['tags']:
-                dt = js['format']['tags']['creation_time']
+    js = ffmpeg.probe(fil, show_entries="format")
+    if "format" in js:
+        if "tags" in js["format"]:
+            if "creation_time" in js["format"]["tags"]:
+                dt = js["format"]["tags"]["creation_time"]
                 return _data_from_datetime(dt)
     # print(json.dumps(js))
     data = try_from_stat(fil)
-    if not data: raise NoDateTime()
+    if not data:
+        raise NoDateTime()
     return data
+
 
 def _get_data_crtwo(fil):
     tags = exifread.process_file(fil)
 
-    if 'EXIF DateTimeOriginal' not in tags:
+    if "EXIF DateTimeOriginal" not in tags:
         print("SKIP (no EXIF DateTimeOriginal): " + f)
         print(tags.keys())
         asfd()
         return
 
-    datetime = tags['EXIF DateTimeOriginal']
-    data = try_datetime({'test': str(datetime)}, 'test')
+    datetime = tags["EXIF DateTimeOriginal"]
+    data = try_datetime({"test": str(datetime)}, "test")
     return data
+
 
 def _get_suffix(f):
     f = f.lower()
-    if f.endswith('.jpg'):
+    if f.endswith(".jpg"):
         return "jpg"
-    if f.endswith('.cr2'):
+    if f.endswith(".cr2"):
         return "CR2"
-    if f.endswith('.3gp'):
+    if f.endswith(".3gp"):
         return "3gp"
-    if f.endswith('.mp4'):
+    if f.endswith(".mp4"):
         return "mp4"
-    if f.endswith('.mpg'):
+    if f.endswith(".mpg"):
         return "mpg"
-    if f.endswith('.mov'):
+    if f.endswith(".mov"):
         return "mov"
-    if f.endswith('.tif'):
+    if f.endswith(".tif"):
         return "tif"
-    if f.endswith('.png'):
+    if f.endswith(".png"):
         return "png"
     raise UnsupportedSuffix()
 
+
 def dofile(f):
     suff = _get_suffix(f)
-    with open(f, 'rb') as image_file:
+    with open(f, "rb") as image_file:
         data = {}
 
         if suff == "jpg":
@@ -174,28 +205,32 @@ def dofile(f):
             raise Exception(f"BUG: Matched suffix but missing impl! {suff}")
 
         if not data:
-            raise Exception(f"BUG: Matched suffix, ran get_data but filed data == None: {f})")
+            raise Exception(
+                f"BUG: Matched suffix, ran get_data but filed data == None: {f})"
+            )
 
         directory = f'../sorted/{data["year"]}/{data["month"]}/{data["day"]}'
         if not os.path.exists(directory):
             os.makedirs(directory)
 
         has_xmp = False
-        xmp_f = f + '.xmp'
+        xmp_f = f + ".xmp"
         if os.path.exists(xmp_f):
             has_xmp = True
 
-        md5sum  = md5(f)
-        newpath = f'{directory}/{data["hour"]}{data["min"]}{data["sec"]}-{md5sum}.{suff}'
-        xmp_newpath = newpath + '.xmp'
+        md5sum = md5(f)
+        newpath = (
+            f'{directory}/{data["hour"]}{data["min"]}{data["sec"]}-{md5sum}.{suff}'
+        )
+        xmp_newpath = newpath + ".xmp"
 
-        prefix = ''
+        prefix = ""
         if DRY:
-            prefix = '[dry] '
+            prefix = "[dry] "
 
         # Dupe found
         if os.path.exists(newpath):
-            print(f'{prefix}Skipped: {f} -> {newpath}')
+            print(f"{prefix}Skipped: {f} -> {newpath}")
             return
 
         # Do move if not dry
@@ -203,7 +238,7 @@ def dofile(f):
             shutil.move(f, newpath)
             if has_xmp:
                 shutil.move(xmp_f, xmp_newpath)
-        print(f'{prefix}Moved:   {f} -> {newpath}')
+        print(f"{prefix}Moved:   {f} -> {newpath}")
 
 
 def main():
@@ -211,7 +246,7 @@ def main():
     if len(sys.argv) == 2 and sys.argv[1] == "--dry":
         DRY = True
 
-    with open('/tmp/tmp.list', 'r') as lst:
+    with open("/tmp/tmp.list", "r") as lst:
         line = lst.readline().strip()
         while line:
             # print('DEBUG: ' + line, flush=True)
@@ -228,5 +263,6 @@ def main():
                     raise e
             line = lst.readline().strip()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
