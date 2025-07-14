97f0903565c144ae3c76d838292acb9d4d12e62d
Ian Page Hands <iphands@gmail.com>
2025-07-14 16:29:01 +0000

    lots of fixes: support videos, move instead of copy, better logging

--- sort.py
+++ sort.py
@@ -1,12 +1,20 @@
-import sys
+import exifread
+import ffmpeg
+import hashlib
+import json
 import os
 import shutil
-import hashlib
-import exifread
-from exif import Image
+import subprocess
+import sys
 
-class NoExif(Exception): pass
+from datetime import datetime
+from exif import Image
+from plum.exceptions import UnpackError
+
 class NoDateTime(Exception): pass
+class NoExif(Exception): pass
+class NoValidDate(Exception): pass
+class UnsupportedSuffix(Exception): pass
 
 DRY=False
 
@@ -32,7 +40,7 @@ def try_gps_date(img):
     return False
 
 def try_datetime(img, key):
-    # if key in dir(img):
+    if key not in img and not key in dir(img): return
     dt   = img[key].split(':')
     data = {
         "year":  dt[0],
@@ -43,16 +51,68 @@ def try_datetime(img, key):
         "sec":   dt[4]
     }
     return data
-    # return False
 
-def _get_data_norm(image_file):
-    my_image = Image(image_file)
+def try_from_stat(img):
+    stat = os.stat(img)
+    dates = [
+        datetime.fromtimestamp(stat.st_ctime),
+        datetime.fromtimestamp(stat.st_mtime),
+        datetime.fromtimestamp(stat.st_atime),
+    ]
+    dates = sorted(dates)
+    date = None
+    min_date = datetime(2001, 1, 1)
+    for i in dates:
+        if i > min_date:
+            date = i
+            break
+    if date == None: raise NoValidDate(f"Cant find valid date in {img}: {dates}")
+    return _data_from_datetime(str(date))
 
-    if not my_image.has_exif: raise NoExif()
+def _get_data_norm(f, image_file):
+    try:
+        my_image = Image(image_file)
+    except UnpackError:
+        data = try_from_stat(f)
+        if not data: raise e
+        return data
+
+    if not my_image.has_exif:
+        data = try_from_stat(f)
+        if not data: raise NoExif()
+        return data
 
     data = try_gps_date(my_image)
     if not data: data = try_datetime(my_image, 'datetime_original')
     if not data: data = try_datetime(my_image, 'datetime')
+    if not data: data = try_from_stat(f)
+    if not data: raise NoDateTime()
+    return data
+
+def _data_from_datetime(dt):
+    dt = dt.split(".")[0]
+    format_code = "%Y-%m-%d %H:%M:%S"
+    if "T" in dt:
+        format_code = "%Y-%m-%dT%H:%M:%S"
+    parsed = datetime.strptime(dt, format_code)
+    return {
+        "year": str(parsed.year).zfill(4),
+        "month": str(parsed.month).zfill(2),
+        "day": str(parsed.day).zfill(2),
+        "hour": str(parsed.hour).zfill(2),
+        "min": str(parsed.minute).zfill(2),
+        "sec": str(parsed.second).zfill(2),
+    }
+
+def _get_data_ffmpeg(fil):
+    js = ffmpeg.probe(fil, show_entries='format')
+    if 'format' in js:
+        if 'tags' in js['format']:
+            if 'creation_time' in js['format']['tags']:
+                dt = js['format']['tags']['creation_time']
+                return _data_from_datetime(dt)
+    # print(json.dumps(js))
+    data = try_from_stat(fil)
     if not data: raise NoDateTime()
     return data
 
@@ -69,18 +129,52 @@ def _get_data_crtwo(fil):
     data = try_datetime({'test': str(datetime)}, 'test')
     return data
 
+def _get_suffix(f):
+    f = f.lower()
+    if f.endswith('.jpg'):
+        return "jpg"
+    if f.endswith('.cr2'):
+        return "CR2"
+    if f.endswith('.3gp'):
+        return "3gp"
+    if f.endswith('.mp4'):
+        return "mp4"
+    if f.endswith('.mpg'):
+        return "mpg"
+    if f.endswith('.mov'):
+        return "mov"
+    if f.endswith('.tif'):
+        return "tif"
+    if f.endswith('.png'):
+        return "png"
+    raise UnsupportedSuffix()
 
-def dofile(f, crtwo=False):
+def dofile(f):
+    suff = _get_suffix(f)
     with open(f, 'rb') as image_file:
         data = {}
-        suff = 'jpg'
-        if crtwo:
-            suff = 'CR2'
 
-        if not crtwo:
-            data = _get_data_norm(image_file)
-        else:
+        if suff == "jpg":
+            data = _get_data_norm(f, image_file)
+        if suff == "tif":
+            data = _get_data_norm(f, image_file)
+        if suff == "png":
+            data = _get_data_norm(f, image_file)
+        elif suff == "CR2":
             data = _get_data_crtwo(image_file)
+        elif suff == "3gp":
+            data = _get_data_ffmpeg(f)
+        elif suff == "mp4":
+            data = _get_data_ffmpeg(f)
+        elif suff == "mpg":
+            data = _get_data_ffmpeg(f)
+        elif suff == "mov":
+            data = _get_data_ffmpeg(f)
+        else:
+            raise Exception(f"BUG: Matched suffix but missing impl! {suff}")
+
+        if not data:
+            raise Exception(f"BUG: Matched suffix, ran get_data but filed data == None: {f})")
 
         directory = f'../sorted/{data["year"]}/{data["month"]}/{data["day"]}'
         if not os.path.exists(directory):
@@ -106,9 +200,9 @@ def dofile(f, crtwo=False):
 
         # Do move if not dry
         if not DRY:
-            shutil.copy(f, newpath)
+            shutil.move(f, newpath)
             if has_xmp:
-                shutil.copy(xmp_f, xmp_newpath)
+                shutil.move(xmp_f, xmp_newpath)
         print(f'{prefix}Moved:   {f} -> {newpath}')
 
 
@@ -123,7 +217,12 @@ def main():
             # print('DEBUG: ' + line, flush=True)
             if len(line) > 4:
                 try:
-                    dofile(line, crtwo=line.endswith('CR2'))
+                    dofile(line)
+                except (NoExif, NoDateTime, UnsupportedSuffix) as e:
+                    if e != "":
+                        print(f"FAIL({type(e).__name__}): {line}")
+                    else:
+                        print(f"FAIL({type(e).__name__}): {e}")
                 except Exception as e:
                     print("FAIL(" + type(e).__name__ + "): " + line)
                     raise e
